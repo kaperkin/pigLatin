4e13ca5a61b55557b096ab2fda03dce9f3350f8d
Kaleena Perkins <kaleena.perkins@gmail.com>
2017-07-14 20:58:27 -0700

    add text shadow to title, refactor function to create sub functions, handle words with one vowel located at start

--- js/app.js
+++ js/app.js
@@ -4,37 +4,7 @@ $('#output').hide();
 var height = $(window).height();   // returns height of browser viewport
 $('#jumbotron').css("height", height-100);
 
-
-function translateWord(str) {
-  var sA = str.split("");
-  var count = str.length;
-  var vowels = ['a','e','i','o','u','y'];
-  var punctuation = [' ','.',',','?','!','@','#','$','%','^','&','*','(',')','_','-','+','=','<','>',':',';','"',
-  "'","{","}",'|','\\','[',']','~','``'];
-  var punc = "";
-  var lastChar = sA.pop();
-  if(punctuation.indexOf(lastChar)!=-1){
-    punc = lastChar;
-  } else{
-    sA.push(lastChar);
-  };
-  //for loop returns index of first vowel
-  for(var i=0; i<vowels.length; i++){
-    if((sA.indexOf(vowels[i]) > -1) && ((sA.indexOf(vowels[i]) < count))){
-      count = sA.indexOf(vowels[i]);
-    }
-  }
-   //end of sA from vowel to end
-  var end = sA.splice(count, sA.length);
-  console.log(end);
-  // front of sA until firs vowel
-  var front = sA.splice(0, count);
-  if(front[0]){
-  if(front[0].toUpperCase()==front[0]){
-    front[0] = front[0].toLowerCase();
-    end[0] = end[0].toUpperCase();
-  }
-}
+function joinWord(end, front, count, punc){
   //string of end + front
   str = end + front;
   //strip out all commas
@@ -44,6 +14,50 @@ function translateWord(str) {
   } else {
   return  (str + 'ay' + punc);
 }
+}
+
+function capitalization(front, end){
+  if(front[0]){
+  if(front[0].toUpperCase()==front[0] && end[0]){
+    front[0] = front[0].toLowerCase();
+    end[0] = end[0].toUpperCase();
+  }
+    return front, end;
+  }
+}
+
+function punctuationHandler(sA){
+  var punctuation = [' ','.',',','?','!','@','#','$','%','^','&','*','(',')','_','-','+','=','<','>',':',';','"',
+  "'","{","}",'|','\\','[',']','~','``'];
+  var punc ='';
+  var lastChar = sA.pop();
+  if(punctuation.indexOf(lastChar)!=-1){
+    punc = lastChar;
+    return [sA, punc];
+  } else{
+     return [sA.push(lastChar), punc];
+  };
+}
+
+
+
+function translateWord(str) {
+  var sA = str.split("");
+  var count = str.length;
+  var vowels = ['a','e','i','o','u','y'];
+  var punc = punctuationHandler(sA)[1];
+  //for loop returns index of first vowel
+  for(var i=0; i<vowels.length; i++){
+    if((sA.indexOf(vowels[i]) > -1) && ((sA.indexOf(vowels[i]) < count))){
+      count = sA.indexOf(vowels[i]);
+    }
+  }
+  //end of sA from vowel to end
+  var end = sA.splice(count, sA.length);
+  // front of sA until firs vowel
+  var front = sA.splice(0, count);
+  capitalization(front, end);
+  return joinWord(end, front, count, punc)
 }
 
 function translateBtnClicked(){
